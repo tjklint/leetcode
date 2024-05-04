bda061c1b2e0d68779f60a8954622d2ed41a3825
TJ Klint <timothyjklint@gmail.com>
2024-05-04 01:43:36 -0400

    Added q724.cs

---
+++ leetcode/q724/q724.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int PivotIndex(int[] nums) {
+        int a = nums.Sum();
+        int b = 0;
+
+        for (int i = 0; i < nums.Length; i++) {
+            a -= nums[i];
+            
+            if (a == b) {
+                return i;
+            }
+          
+            b += nums[i];
+        }
+
+        return -1;
+    }
+}
