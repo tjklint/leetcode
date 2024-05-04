f93c3ed382915ab7bfde654be6bd716b5399732a
TJ Klint <timothyjklint@gmail.com>
2024-05-04 01:49:39 -0400

    Added comments.

--- leetcode/q724/q724.cs
+++ leetcode/q724/q724.cs
@@ -3,14 +3,20 @@ public class Solution {
         int a = nums.Sum();
         int b = 0;
 
+        // Simple loop through the array to find the index.
         for (int i = 0; i < nums.Length; i++) {
+
+            // Remove value from the max.
             a -= nums[i];
+            // If the value matches, return the index.
             if (a == b) {
                 return i;
             }
+            // Add the value to the new sum.
             b += nums[i];
         }
-        
+
+        // If nothing is found, return -1.
         return -1;
     }
 }
